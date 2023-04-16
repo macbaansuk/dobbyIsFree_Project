5317664fec5a8538609044afeb79a92460c3d574
sooheee <tngml2684@naver.com>
2023-04-16 20:51:10 +0900

    예외처리, alert(메세지) 수정

--- src/main/java/com/dobby/project/soo/AdminNoticeController.java
+++ src/main/java/com/dobby/project/soo/AdminNoticeController.java
@@ -19,42 +19,30 @@ public class AdminNoticeController {
     @Autowired
     NoticeService noticeService;
 
-    @GetMapping("/modify")  // 수정하기 위해 기존 데이터 읽어오기
-    public String adminNoticeModifyForm(Integer page, Integer pageSize,Integer NB_ID, Model m ) throws Exception {
-//        System.out.println("NoticeController - NB_ID: " + NB_ID);
-        try {
-            NoticeDto noticeDto = noticeService.read(NB_ID);
-            m.addAttribute("noticeDto",noticeDto);
-            m.addAttribute("page", page);
-            m.addAttribute("pageSize", pageSize);
-            m.addAttribute("mode", "mod");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "/soo/admin-notice-read";
-    }
 
 
-    @PostMapping("/modify") // 수정 후 데이터 보내기
+
+    @PostMapping("/modify")
     public String adminNoticeModify(NoticeDto noticeDto, Integer page, Integer pageSize, RedirectAttributes rattr, Model m) throws Exception {
         try {
             noticeService.modify(noticeDto);
 
             rattr.addAttribute("page", page);
             rattr.addAttribute("pageSize", pageSize);
-            m.addAttribute("mode", "mod");
+
             return "redirect:/admin/notice/list?page=" + page + "&pageSize=" + pageSize;
 
         } catch (Exception e) {
             e.printStackTrace();
-            // 예외가 발생하면, 사용자가 입력하던 내용을 그대로 보여줘야하기 때문에 Model에 noticeDto를 저장하고
             m.addAttribute(noticeDto);
             m.addAttribute("page", page);
             m.addAttribute("pageSize", pageSize);
-            // 뷰에 다시 전달
+
             return "soo/admin-notice-read";
         }
     }
+
+
 
 
     @PostMapping("/remove") // 게시물 수정 페이지에서의 삭제
@@ -71,8 +59,8 @@ public class AdminNoticeController {
         return "redirect:/admin/notice/list";
     }
 
-    @GetMapping("/read") // 게시물 제목 누르면 읽기 전용
-    public String adminNoticeRead(Integer page, Integer pageSize,Integer NB_ID, Model m , RedirectAttributes rattr) throws Exception {
+    @GetMapping("/read")
+    public String adminNoticeRead(Integer page, Integer pageSize,Integer NB_ID, Model m, SearchCondition sc ) throws Exception {
         System.out.println("NoticeController - NB_ID: " + NB_ID);
 
         try {
@@ -80,52 +68,39 @@ public class AdminNoticeController {
             m.addAttribute("noticeDto",noticeDto);
             m.addAttribute("page", page);
             m.addAttribute("pageSize", pageSize);
-            m.addAttribute("mode", "read");
+
         } catch (Exception e) {
             e.printStackTrace();
-            // 게시물 읽기 실패하면, 게시물 목록으로 돌아갈 때 이전 페이지 정보를 유지
-            rattr.addAttribute("page", page);
-            rattr.addAttribute("pageSize", pageSize);
-            return "redirect:/admin/notice/list";
         }
 
         return "/soo/admin-notice-read";
     }
 
-    @GetMapping("/write")   // 게시물 등록 폼
+    @GetMapping("/write")
     public String adminNoticeWriteForm(Model m) {
-
-        try {
-            NoticeDto noticeDto = new NoticeDto();
-            m.addAttribute("noticeDto", new NoticeDto());
-            m.addAttribute("mode", "new");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return "soo/admin-notice-read";
+        NoticeDto noticeDto = new NoticeDto();
+        m.addAttribute("noticeDto", new NoticeDto());
+        return "soo/admin-notice-write";
 
     }
 
 
-    @PostMapping("/write")  // 게시물 등록 후 데이터 보내기
+    @PostMapping("/write")
     public String adminNoticeWrite(Model m, NoticeDto noticeDto) {
         try {
             noticeService.write(noticeDto);
-            m.addAttribute("mode", "new");
 
         } catch (Exception e) {
             e.printStackTrace();
-            // 사용자가 등록하던 내용을 다시 보여줘야함
-            m.addAttribute(noticeDto);
-            return "/soo/admin-notice-read";
+
         }
         return "redirect:/admin/notice/list";
     }
 
 
-    @GetMapping("/list")    // 목록 읽어오기
+    @GetMapping("/list")
     public String adminNoticeList(@RequestParam(defaultValue ="1") Integer page,
-                                  @RequestParam(defaultValue = "10") Integer pageSize,Model m) throws Exception {
+                             @RequestParam(defaultValue = "10") Integer pageSize,Model m, HttpServletRequest request) throws Exception {
 
         try {
             int totalCnt = noticeService.getCount();
