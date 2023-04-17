175b517a69bbd58a9fb0c7166e154e530bcac410
sooheee <tngml2684@naver.com>
2023-04-17 16:03:52 +0900

    카테고리별 게시물 조회 구현 초기

--- src/main/java/com/dobby/project/soo/NoticeServiceImpl.java
+++ src/main/java/com/dobby/project/soo/NoticeServiceImpl.java
@@ -1,6 +1,5 @@
 package com.dobby.project.soo;
 
-import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -19,32 +18,42 @@ public class NoticeServiceImpl implements NoticeService {
     }
 
     // 나중에 String WRTR 추가하기, 관리자 계정(이름)
-    @Override
+    @Override // 게시물 삭제
     public int remove(Integer NB_ID) throws Exception{
         return noticeDao.delete(NB_ID);
     }
-    @Override
+    @Override // 게시물 작성
     public int write(NoticeDto noticeDto) throws Exception{
 
         return noticeDao.insert(noticeDto);
     }
-    @Override
+    @Override // 모든 게시물 조회
     public List<NoticeDto> getList() throws Exception{
         return noticeDao.selectAll();
     };
-    @Override
+    @Override  // 게시물 읽기
     public NoticeDto read(Integer NB_ID) throws Exception{
         NoticeDto noticeDto = noticeDao.select(NB_ID);
 //        noticeDao.increaseViewCnt(NB_ID);
         return noticeDto;
     }
-    @Override
+    @Override  // 페이지별 게시물 읽기
     public List<NoticeDto> getPage(Map map) throws Exception{
         return noticeDao.selectPage(map);
     }
-    @Override
+    @Override// 게시물 수정
     public int modify(NoticeDto noticeDto) throws Exception{
         return noticeDao.update(noticeDto);
+    }
+
+    @Override  // 카테고리별 게시물 조회
+    public List<NoticeDto> getListByCate(String BBS_CATE)throws Exception{
+        return noticeDao.selectByCate(BBS_CATE);
+    }
+
+    @Override // 카테고리별 게시물 개수
+    public int getCountByCate(String BBS_CATE) throws Exception{
+        return noticeDao.countByCate(BBS_CATE);
     }
 
     @Override
