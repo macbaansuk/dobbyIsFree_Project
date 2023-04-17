06375674156cf25cca84af90b6e43532f8abf504
sooheee <tngml2684@naver.com>
2023-04-17 16:03:52 +0900

    카테고리별 게시물 조회 구현 초기

--- src/main/java/com/dobby/project/soo/NoticeDao.java
+++ src/main/java/com/dobby/project/soo/NoticeDao.java
@@ -4,31 +4,45 @@ import java.util.List;
 import java.util.Map;
 
 public interface NoticeDao {
-    NoticeDto select(Integer NB_ID) throws Exception;   //게시물 번호로 게시물을 조회
 
-    // 나중에 String WRTR 추가하기, 관리자 계정(이름)
-    int delete(Integer NB_ID) throws Exception;    //게시물 번호, 작성자로 게시물 삭제
+    //게시물 번호로 게시물을 조회
+    NoticeDto select(Integer NB_ID) throws Exception;
 
-    int insert(NoticeDto dto) throws Exception;     // 게시물 작성
+    //게시물 삭제, 나중에 String WRTR 추가하기, 관리자 계정(이름)
+    int delete(Integer NB_ID) throws Exception;
 
-    int update(NoticeDto dto) throws Exception;     // 게시물 수정
+    // 게시물 작성
+    int insert(NoticeDto dto) throws Exception;
 
-    int increaseViewCnt(Integer NB_ID) throws Exception;    // 게시물별 조회수
+    // 게시물 수정
+    int update(NoticeDto dto) throws Exception;
 
-    List<NoticeDto> selectPage(Map map) throws Exception;   // 페이지 단위로 게시물 조회, Map = 페이지번호, 페이지 당 게시물 수
+    // 게시물별 조회수
+    int increaseViewCnt(Integer NB_ID) throws Exception;
 
-    List<NoticeDto> selectAll() throws Exception;       // 모든 게시물 조회
+    // 페이지 단위로 게시물 조회, Map = 페이지번호, 페이지 당 게시물 수
+    List<NoticeDto> selectPage(Map map) throws Exception;
 
-    List<NoticeDto> selectByCate(String BBS_CATE) throws Exception;     // 카테고리별 게시물 조회
+    // 모든 게시물 조회
+    List<NoticeDto> selectAll() throws Exception;
 
-    int deleteAll() throws Exception;   // 모든 게시물 삭제
+    // 카테고리별 게시물 조회
+    List<NoticeDto> selectByCate(String BBS_CATE) throws Exception;
 
-    int countAll() throws Exception;   // 게시물 전체 개수 조회
+    // 게시물 전체 개수
+    int countAll() throws Exception;
 
-    int countByCate(String BBS_CATE) throws Exception;  // 카테고리별 게시물 개수 조회
+    // 카테고리별 게시물 개수
+    int countByCate(String BBS_CATE) throws Exception;
 
-    int searchResultCnt(SearchCondition sc) throws Exception;   // 검색 결과의 총 개수
-    List<NoticeDto> searchSelectPage(SearchCondition sc) throws Exception;  // 검색 결과 목록
+    // 모든 게시물 삭제
+    int deleteAll() throws Exception;
+
+    // 검색 결과의 총 개수
+    int searchResultCnt(SearchCondition sc) throws Exception;
+
+    // 검색 결과 목록
+    List<NoticeDto> searchSelectPage(SearchCondition sc) throws Exception;
 
 
 }
