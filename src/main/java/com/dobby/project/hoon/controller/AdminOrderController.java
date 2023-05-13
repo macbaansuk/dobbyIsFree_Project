5a030a1a14ab3f04836fd38d66e07ab0860ff966
muzihoon <youngsh92@gamil.com>
2023-05-13 19:18:33 +0900

    관리자 주문목록 페이지 업데이트 1. 컨트롤러 코드 수정 2. 컨트롤러 삭제

--- src/main/java/com/dobby/project/hoon/controller/AdminOrderController.java
+++ src/main/java/com/dobby/project/hoon/controller/AdminOrderController.java
@@ -55,55 +55,55 @@ public class AdminOrderController {
             return "redirect:/admin/login";
         } else { // 본인 페이지 원래 코드
 
-            List<OrdProdInfoDto> opiDto = new ArrayList<>();
-            OrdProdInfoDto opi = new OrdProdInfoDto();
-            opi.setREP_IMG("/img/sun/product-image/skincare/derma_cream/derma_cream_main.jpg");
-            opi.setPROD_NM("더마 그린티 프로바이오틱스 크림");
-            opi.setPROD_INDV_QTY(1);
-            opi.setPROD_AMT(25000);
-            opi.setORD_STUS_CD("상품준비중");
-            opi.setDC_YN("Y");
-            opiDto.add(opi);
-
-            List<OrdDto> ordDto = new ArrayList<>();
-            OrdDto ord = new OrdDto();
-            ord.setORD_ID("20230511163371");
-            ord.setMBR_ID("test1");
-            ord.setCOU_ID(null);
-            ord.setORD_DTM("2023-05-11 14:04:35");
-            ord.setREP_PROD_NM(null);
-            ord.setPROD_CNT(null);
-            ord.setSETL_AMT(22500);
-            ord.setDEXP(0);
-            ord.setDEXP_YN(null);
-            ord.setPROD_OPT(null);
-            ord.setPROD_DC(null);
-            ord.setUSE_COUPN_NO(null);
-            ord.setORD_TAMT(22500);
-            ord.setREVW_YN(null);
-            ord.setORD_STUS_CD("상품준비중");
-            ord.setORD_CNCL_YN(null);
-            ord.setORD_PROCR(null);
-            ord.setORD_RMK(null);
-            ord.setRCPR_NM("김도비");
-            ord.setRCPR_ZPCD("13561");
-            ord.setRCPR_BASIC_ADDR("경기도 성남시 분당구 정자일로 95");
-            ord.setRCPR_DTL_ADDR("1층");
-            ord.setDLVPN_REQ("");
-            ord.setRCPR_MPNO("01012344321");
-            ord.setBANK("은행을 선택해 주세요.");
-            ord.setACC_NO("");
-            ord.setDPOSR("");
-            ord.setORDR_EMAIL("aaa@aaa.com");
-            ord.setORDR("테스트1");
-            ord.setORDR_NO("01012341234");
-            ord.setPOINT_AMT(225);
-            ord.setDC_AMT(0);
-            ordDto.add(ord);
-
-            // 값을 문자열로 변환하여 보내줌
-            model.addAttribute("opiDto",opiDto);
-            model.addAttribute("ordDto",ordDto);
+//            List<OrdProdInfoDto> opiDto = new ArrayList<>();
+//            OrdProdInfoDto opi = new OrdProdInfoDto();
+//            opi.setREP_IMG("/img/sun/product-image/skincare/derma_cream/derma_cream_main.jpg");
+//            opi.setPROD_NM("더마 그린티 프로바이오틱스 크림");
+//            opi.setPROD_INDV_QTY(1);
+//            opi.setPROD_AMT(25000);
+//            opi.setORD_STUS_CD("상품준비중");
+//            opi.setDC_YN("Y");
+//            opiDto.add(opi);
+//
+//            List<OrdDto> ordDto = new ArrayList<>();
+//            OrdDto ord = new OrdDto();
+//            ord.setORD_ID("20230511163371");
+//            ord.setMBR_ID("test1");
+//            ord.setCOU_ID(null);
+//            ord.setORD_DTM("2023-05-11 14:04:35");
+//            ord.setREP_PROD_NM(null);
+//            ord.setPROD_CNT(null);
+//            ord.setSETL_AMT(22500);
+//            ord.setDEXP(0);
+//            ord.setDEXP_YN(null);
+//            ord.setPROD_OPT(null);
+//            ord.setPROD_DC(null);
+//            ord.setUSE_COUPN_NO(null);
+//            ord.setORD_TAMT(22500);
+//            ord.setREVW_YN(null);
+//            ord.setORD_STUS_CD("상품준비중");
+//            ord.setORD_CNCL_YN(null);
+//            ord.setORD_PROCR(null);
+//            ord.setORD_RMK(null);
+//            ord.setRCPR_NM("김도비");
+//            ord.setRCPR_ZPCD("13561");
+//            ord.setRCPR_BASIC_ADDR("경기도 성남시 분당구 정자일로 95");
+//            ord.setRCPR_DTL_ADDR("1층");
+//            ord.setDLVPN_REQ("");
+//            ord.setRCPR_MPNO("01012344321");
+//            ord.setBANK("은행을 선택해 주세요.");
+//            ord.setACC_NO("");
+//            ord.setDPOSR("");
+//            ord.setORDR_EMAIL("aaa@aaa.com");
+//            ord.setORDR("테스트1");
+//            ord.setORDR_NO("01012341234");
+//            ord.setPOINT_AMT(225);
+//            ord.setDC_AMT(0);
+//            ordDto.add(ord);
+//
+//            // 값을 문자열로 변환하여 보내줌
+//            model.addAttribute("opiDto",opiDto);
+//            model.addAttribute("ordDto",ordDto);
 
             return "hoon/AdminorderDetailList";
         }
